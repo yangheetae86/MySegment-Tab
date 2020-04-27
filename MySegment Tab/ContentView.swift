f20db91bf6211a1c72b7faaec3a538e5e92ae438
heroparkjisung2 <heroparkjisung2@gmail.com>
2020-04-27 19:29:24 +0900

    Initial Commit

---
+++ MySegment Tab/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MySegment Tab
+//
+//  Created by JU HAN LEE on 2020/04/27.
+//  Copyright © 2020 yht. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
