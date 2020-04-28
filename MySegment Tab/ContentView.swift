ab175200e61beda82005e039d1e79dfbede6b6fc
heroparkjisung2 <heroparkjisung2@gmail.com>
2020-04-28 16:44:58 +0900

    add

--- MySegment Tab/ContentView.swift
+++ MySegment Tab/ContentView.swift
@@ -28,7 +28,7 @@ struct Home : View {
     var body: some View {
         VStack(spacing: 0) {
             AppBar(index: self.$index, offset: self.$offset)
-            Spacer()
+            First()
         }
         .edgesIgnoringSafeArea(.all)
     }
@@ -41,64 +41,115 @@ struct AppBar : View {
     
     var body: some View {
         
-        HStack {
-            Button(action: {
-                
-                self.index = 1
-            }) {
-                VStack(spacing: 8) {
-                    HStack(spacing: 8) {
-                        Image("Home")
-                            .foregroundColor(self.index == 1 ? .white : Color.white.opacity(0.6))
-                        Text("Home")
-                            .foregroundColor(self.index == 1 ? .white : Color.white.opacity(0.6))
-                    }
-                    Capsule()
-                        .fill(self.index == 1 ? Color.white : Color.clear)
-                        .frame(height: 4)
+        VStack(alignment: .leading, content: {
+            Text("Home")
+                .font(.title)
+                .foregroundColor(.white)
+                .padding(.leading)
+                .padding(.bottom)
+            
+            HStack {
+                Button(action: {
                     
+                    self.index = 1
+                }) {
+                    VStack(spacing: 8) {
+                        HStack(spacing: 8) {
+                            Image("Home")
+                                .foregroundColor(self.index == 1 ? .white : Color.white.opacity(0.6))
+                            Text("Home")
+                                .foregroundColor(self.index == 1 ? .white : Color.white.opacity(0.6))
+                        }
+                        Capsule()
+                            .fill(self.index == 1 ? Color.white : Color.clear)
+                            .frame(height: 4)
+                        
+                    }
+                }
+                
+                Button(action: {
+                    
+                    self.index = 2
+                }) {
+                    VStack(spacing: 8) {
+                        HStack(spacing: 12) {
+                            Image("Search")
+                                .foregroundColor(self.index == 2 ? .white : Color.white.opacity(0.6))
+                            Text("Search")
+                                .foregroundColor(self.index == 2 ? .white : Color.white.opacity(0.6))
+                        }
+                        Capsule()
+                            .fill(self.index == 2 ? Color.white : Color.clear)
+                            .frame(height: 4)
+                        
+                    }
+                }
+                
+                Button(action: {
+                        
+                    self.index = 3
+                }) {
+                    VStack(spacing: 8) {
+                        HStack(spacing: 12) {
+                            Image("Profile")
+                                .foregroundColor(self.index == 3 ? .white : Color.white.opacity(0.6))
+                            Text("Account")
+                                .foregroundColor(self.index == 3 ? .white : Color.white.opacity(0.6))
+                        }
+                        Capsule()
+                            .fill(self.index == 3 ? Color.white : Color.clear)
+                            .frame(height: 4)
+                        
+                    }
                 }
             }
+        })
+        
+        .padding(.top, (UIApplication.shared.windows.first?.safeAreaInsets.top)! + 15)//yh
+        .padding(.horizontal)
+        .padding(.bottom, 10)
+        .background(Color.red)
+    }
+}
+
+struct First : View {
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: false) {
             
-            Button(action: {
-                
-                self.index = 2
-            }) {
-                VStack(spacing: 8) {
-                    HStack(spacing: 12) {
-                        Image("Search")
-                            .foregroundColor(self.index == 2 ? .white : Color.white.opacity(0.6))
-                        Text("Search")
-                            .foregroundColor(self.index == 2 ? .white : Color.white.opacity(0.6))
-                    }
-                    Capsule()
-                        .fill(self.index == 2 ? Color.white : Color.clear)
-                        .frame(height: 4)
-                    
-                }
-            }
-            
-            Button(action: {
-                    
-                self.index = 3
-            }) {
-                VStack(spacing: 8) {
-                    HStack(spacing: 12) {
-                        Image("Profile")
-                            .foregroundColor(self.index == 3 ? .white : Color.white.opacity(0.6))
-                        Text("Account")
-                            .foregroundColor(self.index == 3 ? .white : Color.white.opacity(0.6))
-                    }
-                    Capsule()
-                        .fill(self.index == 3 ? Color.white : Color.clear)
-                        .frame(height: 4)
-                    
+            VStack(spacing:0) {
+                ForEach(1..<6, id: \.self) {i in
+                    Image("p\(i)")
+                    .resizable()
+                    .frame(height: 250)
+                    .cornerRadius(15)
+                    .padding(.top)
+                    .padding(.horizontal)
                 }
             }
         }
-        .padding(.top, (UIApplication.shared.windows.first?.safeAreaInsets.top)! + 15)//yh
-        .padding(.horizontal)
-        .padding(.bottom, 8)
-        .background(Color.red)
+        .padding(.bottom, 18)
+    }
+}
+
+struct Scnd : View 	{
+    var body: some View {
+        GeometryReader {_ in
+            
+            VStack {
+                Text("Scnd View")
+            }
+        }
+        .background(Color.white)
+    }
+}
+
+struct Third : View {
+    var body: some View {
+        GeometryReader {_ in
+            VStack {
+                Text("Third View")
+            }
+        }
+        .background(Color.white)
     }
 }
